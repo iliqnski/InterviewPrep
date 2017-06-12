f84ce98dfbc529bf2563da9bafbab8a49134de36
Iliayn Hristov <iliqn408@abv.bg>
2017-06-12 20:59:53 +0300

    Work in progress.

---
+++ Tests/Tests/Tests/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace Tests
+{
+    using System;
+
+    public class Program
+    {
+        static void Main()
+        {
+            //delegate instance
+            WorkPerformedHandler delegate1 = 
+                new WorkPerformedHandler(WorkPerformed1);
+
+            WorkPerformedHandler delegate2 =
+               new WorkPerformedHandler(WorkPerformed2);
+
+            WorkPerformedHandler delegate3 =
+               new WorkPerformedHandler(WorkPerformed3);
+
+            delegate1 += delegate2 + delegate3;
+            int hours = delegate1(10, WorkType.Player);
+            Console.WriteLine(hours);
+            //DoWork(delegate3);
+
+        }
+
+        //handler
+        static int WorkPerformed1(int workHours, WorkType type)
+        {
+            System.Console.WriteLine
+                ("WorkPerformed1 called!" + workHours.ToString());
+
+            return workHours + 1;
+        }
+
+        //handler
+        static int WorkPerformed2(int workHours, WorkType type)
+        {
+            System.Console.WriteLine
+                ("WorkPerformed2 called!" + workHours.ToString());
+
+            return workHours + 1;
+        }
+
+        //handler
+        static int WorkPerformed3(int workHours, WorkType type)
+        {
+            System.Console.WriteLine
+                ("WorkPerformed3 called!" + workHours.ToString());
+
+            return workHours + 1;
+        }
+
+        static void DoWork(WorkPerformedHandler del)
+        {
+            del(5, WorkType.Player);
+        }
+    }
+
+    public enum WorkType
+    {
+        Golf,
+        GenerateReports,
+        Player
+    }
+}
---
+++ Tests/Tests/Tests/Worker.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Tests
+{
+    using System;
+
+    //delegate 
+    public delegate int WorkPerformedHandler
+        (int hours, WorkType type);
+
+    public class Worker
+    {
+        public event WorkPerformedHandler WorkPerformed;
+        public event EventHandler WorkCompleted;
+
+        public void DoWork(int hours, WorkType type)
+        {
+
+        }
+    }
+}

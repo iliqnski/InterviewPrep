22cd2709311eaf197322ccae66f631615b8b1a45
Iliyan Hristov <iliyan.h@sbtech.com>
2017-06-23 09:18:40 +0300

    Work in progress

---
+++ Tests/DesignPatterns/DesignPatterns/Creational/AbstractFactory.cs
@@ -0,0 +1,41 @@
+﻿namespace DesignPatterns.Creational
+{
+    using System;
+
+    class AbstractFactory
+    {
+
+    }
+
+    abstract class ContinentFactory
+    {
+        public abstract Herbivore CreateHerbivore();
+        public abstract Carnivore CreateCarnivore();
+    }
+
+    class AfricaFactory : ContinentFactory
+    {
+        public override Carnivore CreateCarnivore()
+        {
+            throw new WildeBeest();
+        }
+
+        public override Herbivore CreateHerbivore()
+        {
+            throw new Lion();
+        }
+    }
+
+    class AmericaFactory : ContinentFactory
+    {
+        public override Herbivore CreateHerbivore()
+        {
+            throw new Bison();
+        }
+
+        public override Carnivore CreateCarnivore()
+        {
+            throw new Wolf();
+        }
+    }
+}
---
+++ Tests/DesignPatterns/DesignPatterns/Creational/FactoryMethod.cs
@@ -0,0 +1,57 @@
+﻿namespace DesignPatterns.Creational
+{
+    using System;
+
+    public class FactoryMethod
+    {
+
+    }
+
+    public abstract class Manufacturer
+    {
+        public abstract Gsm ManufactureGsm();
+    }
+
+    public class PearComputers : Manufacturer
+    {
+        public override Gsm ManufactureGsm()
+        {
+            var phone = new EyePhone { Height = 200, Weight = 100 };
+            return phone;
+        }
+    }
+    public class SamunComputers : Manufacturer
+    {
+        public override Gsm ManufactureGsm()
+        {
+            var phone = new SamunGalaxy { Height = 199, Weight = 99 };
+            return phone;
+        }
+    }
+
+
+    public abstract class Gsm
+    {
+        public string Name { get; set; }
+
+        public int Weight { get; set; }
+
+        public int Height { get; set; }
+    }
+
+    public class EyePhone : Gsm
+    {
+        public EyePhone()
+        {
+            this.Name = "EyePhone";
+        }
+    }
+
+    public class SamunGalaxy : Gsm
+    {
+        public SamunGalaxy()
+        {
+            this.Name = "Samun Galaxy";
+        }
+    }
+}

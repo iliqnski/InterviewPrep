e1a217607231e407858c65b9b9922fbd4fc51db9
Iliayn Hristov <iliqn408@abv.bg>
2017-06-25 14:31:53 +0300

    Work in progress

---
+++ Tests/AspNetFundamentals/AspNetLifeCycle/MyHandler.cs
@@ -0,0 +1,21 @@
+﻿namespace AspNetLifeCycle
+{
+    using System;
+    using System.Web;
+
+    public class MyHandler : IHttpHandler
+    {
+        public bool IsReusable
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.Write("My Handler called.");
+        }
+    }
+}

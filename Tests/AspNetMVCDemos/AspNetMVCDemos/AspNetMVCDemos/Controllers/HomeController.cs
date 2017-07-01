6c1e46404ef74d78a360ca1cc4c204195c60ea43
Iliayn Hristov <iliqn408@abv.bg>
2017-07-01 11:21:35 +0300

    Work in progress

---
+++ Tests/AspNetMVCDemos/AspNetMVCDemos/AspNetMVCDemos/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspNetMVCDemos.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult ServerTime()
+        {
+            var isAjax = Request.IsAjaxRequest();
+            Thread.Sleep(2000);
+            return this.Content(isAjax + " " + DateTime.Now.ToLongTimeString());
+        }
+
+        public ActionResult AjaxActionLink()
+        {
+            return this.View();
+        }
+    }
+}

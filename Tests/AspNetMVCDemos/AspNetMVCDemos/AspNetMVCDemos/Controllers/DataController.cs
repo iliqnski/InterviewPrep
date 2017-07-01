8ed77a6e956ed68e5bee88b0cc30f11e0e351bb6
Iliayn Hristov <iliqn408@abv.bg>
2017-07-01 22:28:03 +0300

    Work in progress

---
+++ Tests/AspNetMVCDemos/AspNetMVCDemos/AspNetMVCDemos/Controllers/DataController.cs
@@ -0,0 +1,30 @@
+﻿using AspNetMVCDemos.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspNetMVCDemos.Controllers
+{
+    public class DataController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return this.View();
+        }
+
+        //[HttpPost]
+        //public ActionResult Index(int number, string text)
+        //{
+        //    return this.View();
+        //}
+
+        [HttpPost]
+        public ActionResult Index(PersonalDataViewModel model)
+        {
+            return this.View();
+        }
+    }
+}
--- Tests/AspNetMVCDemos/AspNetMVCDemos/AspNetMVCDemos/Controllers/HomeController.cs
+++ Tests/AspNetMVCDemos/AspNetMVCDemos/AspNetMVCDemos/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using AspNetMVCDemos.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading;
 using System.Web;
 using System.Web.Mvc;
@@ -14,20 +16,6 @@ namespace AspNetMVCDemos.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-
         public ActionResult ServerTime()
         {
             var isAjax = Request.IsAjaxRequest();
@@ -38,6 +26,47 @@ namespace AspNetMVCDemos.Controllers
         public ActionResult AjaxActionLink()
         {
             return this.View();
+        }
+
+        public ActionResult Books()
+        {
+            var data = BooksData
+                .GetAll()
+                .AsQueryable()
+                .Select(BookViewModel.FromBook)
+                .ToList();
+
+            return this.View(data);
+        }
+
+        public ActionResult Search(string query)
+        {
+            var result = BooksData
+                .GetAll()
+                .AsQueryable()
+                .Where(b => b.Title.ToLower().Contains(query.ToLower()))
+                .Select(BookViewModel.FromBook)
+                .ToList();
+
+            return this.PartialView("_BookResult", result);
+        }
+
+        public ActionResult ContentById(int id)
+        {
+            if (!Request.IsAjaxRequest())
+            {
+                Response.StatusCode = (int)HttpStatusCode.Forbidden;
+                return this.Content("Not permeted, only Ajax call");
+            }
+
+            var book = BooksData.GetAll().FirstOrDefault(x => x.Id == id);
+            if (book == null)
+            {
+                Response.StatusCode = (int)HttpStatusCode.NotFound;
+                return this.Content("Book is not found");
+            }
+
+            return this.Content(book.Content);
         }
     }
 }
